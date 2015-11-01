f5710d9cf6648708e5530b7a21ac0f1afd7e3d75
aldo.ea <aldo.ea@hotmail.com>
2015-11-01 07:57:28 -0500

    First empuje

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  res.send('home');
 });
 
 module.exports = router;
