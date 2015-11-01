a293714bf44936b71aed96403c0382a38cb74729
aldo.ea <aldo.ea@hotmail.com>
2015-11-01 11:27:26 -0500

    Morning Pushing

--- public/Angular/app/js/app.js
+++ public/Angular/app/js/app.js
@@ -1,7 +1,26 @@
-var app = angular.module('flapperNews', []);
+window.onload = function() { document.body.className = ''; }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-  console.log("Hello angular!")
+  console.log("Hello angular!");
 }]);
