e461f1b96816f883d99a14fa152df71852a95ea8
aldo.ea <aldo.ea@hotmail.com>
2015-11-01 11:00:27 -0500

    Morning Pushing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var starting = requiere('./routes/start');
+
 var app = express();
 
 // view engine setup
@@ -23,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/start',starting);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
